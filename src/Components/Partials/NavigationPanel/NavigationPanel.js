96d947d4f7695dbe56ec7870a7fdf7b27b46b3cb
ngodi <albertngdi@gmail.com>
2020-09-10 07:14:24 +0100

    responsive nav

--- src/Components/Partials/NavigationPanel/NavigationPanel.js
+++ src/Components/Partials/NavigationPanel/NavigationPanel.js
@@ -4,12 +4,29 @@ import './NavigationPanel.css';
 
 export default () => {
   return(
-      <div className="navigation-panel">
-          <a href="#">Doctor</a><span>|</span>
-          <a href="#">Hospital</a><span>|</span>
-          <a href="#">Unit</a><span>|</span>
-          <a href="#">Emergency</a><span>|</span>
-          <a href="#">Laboratory</a>
+      <nav className="navigation-panel navbar navbar-expand-md">
+         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+         <i className="fas fa-bars"></i>
+        </button>
+        <div className="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul className="navbar-nav">
+        <li className="nav-item">
+              <a className="nav-link" href="#">Doctor</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Hospital</a>
+            </li>
+           <li className="nav-item">
+              <a className="nav-link" href="#">Unit</a>
+            </li>
+           <li className="nav-item">
+              <a className="nav-link" href="#">Emergency</a>
+            </li>
+           <li className="nav-item">
+             <a className="nav-link" href="#">Laboratory</a>
+           </li>
+        </ul>
       </div>
+      </nav>
   )
 }
